c761ae137c4027f5e8826a57a5578f26019b2596
Sonderman <alihaydar338@gmail.com>
2020-09-02 14:17:58 +0300

    Path Following With Singleton added.

---
+++ Assets/PathFWithSingleton/Araba.cs
@@ -0,0 +1,109 @@
+﻿using UnityEngine;
+
+public class Araba : MonoBehaviour
+{
+    Vector3 targetPosition;
+    SingletonPath path;
+    public float moveSpeed = 5f;
+    public bool looping=true;
+    public bool isBlueCar;
+    bool isPathSwitched=false;
+    private float time = 0.0f;
+    public float repeatTime = 0.1f;
+    void Start()
+    {
+        path = SingletonPath.Instance;
+        if (isBlueCar)
+        {
+            targetPosition = path.getBluePoint(isPathSwitched);
+        }
+        else
+        {
+            targetPosition = path.getRedPoint(isPathSwitched);
+        }
+        
+    }
+
+    
+    void Update()
+    {
+        time += Time.deltaTime;
+        float distance = Vector3.Distance(targetPosition, transform.position);
+        if(distance < 0.1f)
+        {
+            SetNewTargetPosition();
+        }
+        SetLookRotation();
+        GotoTarget();
+    }
+
+    private void SetLookRotation()
+    {
+        Vector3 dir = targetPosition - transform.position;
+        dir.Normalize();
+        Quaternion lookRotation = Quaternion.LookRotation(dir);
+        transform.rotation = Quaternion.Lerp(transform.rotation,lookRotation,Time.deltaTime * 10f);
+    }
+
+    private void GotoTarget()
+    {
+
+        if (isBlueCar)
+        {
+            if (Input.GetKeyDown(KeyCode.Space))
+            {
+                isPathSwitched = !isPathSwitched;
+                path.switchSides(isBlueCar);
+            }
+        }
+        else
+        {
+            repeatTime = Random.Range(3f, 5f);
+            if (time >= repeatTime)
+            {
+                time = 0.0f;
+                isPathSwitched = !isPathSwitched;
+                path.switchSides(isBlueCar);
+            }
+
+        }
+        if (Vector3.Distance(targetPosition, transform.position) < 0.1f) return;
+        Vector3 dir = targetPosition - transform.position;
+        dir.Normalize();
+        transform.position += dir * moveSpeed *Time.deltaTime;
+    }
+
+    private void SetNewTargetPosition()
+    {
+        
+            if (isBlueCar)
+            {
+                targetPosition = path.getBluePoint(isPathSwitched);
+            }
+            else
+            {
+                targetPosition = path.getRedPoint(isPathSwitched);
+            }
+       
+         
+        if (looping)
+        {
+            if (isBlueCar) {
+                if (path.BlueIndex == path.blueGetLength() - 1)
+                {
+                    path.resetIndex(isBlueCar);
+                }
+            } else
+            {
+                if (path.RedIndex == path.redGetLength() - 1)
+                {
+                    path.resetIndex(isBlueCar);
+                }
+            }
+            
+        }
+
+        path.updateIndex(isBlueCar,isPathSwitched);
+
+    }
+}
---
+++ Assets/PathFWithSingleton/SingletonPath.cs
@@ -0,0 +1,106 @@
+﻿using UnityEngine;
+
+public class SingletonPath : MonoBehaviour
+{
+
+    public static SingletonPath Instance { get; private set;}
+
+    public LineRenderer lineRendererBlue;
+    public LineRenderer lineRendererRed;
+
+    public Transform[] bluePathWaypoints;
+    Vector3[] bluePathPositions;
+
+    public Transform[] redPathWaypoints;
+    Vector3[] redPathPositions;
+
+    public int BlueIndex = 0;
+    public int RedIndex = 0;
+
+    private void Awake()
+    {
+        if (Instance== null)
+        {
+            Instance = this;
+            DontDestroyOnLoad(gameObject);
+        }else
+        {
+            //Sahne değişiminde oluşacak olan kopya nesneyi yok eder.
+            Destroy(gameObject);
+        }
+    }
+    private void Start()
+    {
+        lineRendererBlue.startColor = Color.blue;
+        lineRendererBlue.endColor = Color.blue;
+        lineRendererRed.startColor = Color.red;
+        lineRendererRed.endColor = Color.red;
+
+            bluePathPositions = new Vector3[bluePathWaypoints.Length];
+            lineRendererBlue.positionCount = bluePathWaypoints.Length;
+            for (int i = 0; i < bluePathWaypoints.Length; i++)
+            {
+            bluePathPositions[i] = bluePathWaypoints[i].position;
+            lineRendererBlue.SetPositions(bluePathPositions);
+            }
+
+            redPathPositions = new Vector3[redPathWaypoints.Length];
+            lineRendererRed.positionCount = redPathWaypoints.Length;
+            for (int i = 0; i < redPathWaypoints.Length; i++)
+            {
+            redPathPositions[i] = redPathWaypoints[i].position;
+            lineRendererRed.SetPositions(redPathPositions);
+            }
+    }
+    public Vector3 getBluePoint(bool isPathSwitched)
+    {
+        if (isPathSwitched)
+        {
+            return redPathPositions[redPathWaypoints.Length - BlueIndex-1];
+        }
+      return bluePathPositions[BlueIndex];
+    }
+    public Vector3 getRedPoint(bool isPathSwitched)
+    {
+        if (isPathSwitched)
+        {
+            return bluePathPositions[bluePathWaypoints.Length - RedIndex - 1];
+        }
+            return redPathPositions[RedIndex];
+    }
+    public int blueGetLength()
+    {
+        return bluePathWaypoints.Length;
+    }
+    public int redGetLength()
+    {
+        return redPathWaypoints.Length;
+    }
+    public void updateIndex(bool isBlueCar, bool isPathSwitched)
+    {
+        if (isBlueCar)
+        {
+            BlueIndex++;
+        }
+        else RedIndex++;
+    }
+    public void resetIndex(bool isBlueCar)
+    {
+        if (isBlueCar)
+        {
+            BlueIndex=-1;
+        }
+        else RedIndex=-1;
+    }
+    public void switchSides(bool isBlueCar)
+    {
+        if (isBlueCar)
+        {
+            BlueIndex = RedIndex;
+        }
+        else
+        {
+            RedIndex = BlueIndex;
+        }
+    }
+}
